0294eeaf2ac1750f5b959241090491eb4fa5a111
GriffinMLN <65615065+GriffinMLN@users.noreply.github.com>
2020-10-05 01:55:14 +0300

    Saved some lines
    Testing pull request

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 let playerPoint = 0;
 let computerPoints = 0;
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
 
 function computerPlay() {
-    let choose = ["Rock", "Paper", "Scissors"]
+    let choose = [
+        ROCK,
+		PAPER,
+		SCISSORS
+    ]
     let chosenNumber = Math.floor(Math.random() * 4);
     return choose[chosenNumber];
 }
@@ -11,38 +18,21 @@ function playRound(playerSelection, computerSelection) {
     let player = playerSelection.toLowerCase();
     let computer = computerSelection.toLowerCase();
 
-    if (player == "Rock") {
-        if (computer == "Rock") { return "Tie."; }
-        else if (computer == "Paper") {
-            computerPointsIncrease();
-            return "Computer Wins.";
-        }
-        else if (computer == "Scissors") {
-            playerPointsIncrease();
-            return "Player Wins.";
-        }
-    } else if (player == "Paper") {
-        if (computer == "Rock") {
-            playerPointsIncrease();
-            return "Player Wins.";
-        }
-        else if (computer == "Paper") { return "Tie."; }
-        else if (computer == "Scissors") {
-            computerPointsIncrease();
-            return "Computer Wins.";
-        }
-    } else if (player == "Scissors") {
-        if (computer == "Rock") {
-            computerPointsIncrease();
-            return "Computer Wins.";
-        }
-        else if (computer == "Paper") {
-
-            playerPointsIncrease();
-            return "Player Wins.";
-        }
-        else if (computer == "Scissors") { return "Tie."; }
-    }
+	if (player === computer) {
+		return 'Tie.';
+	}
+	else if (
+		(computer === ROCK && player === SCISSORS) ||
+		(computer === PAPER && player === ROCK) ||
+		(computer === SCISSORS && player === PAPER)
+	) {
+		computerPointsIncrease();
+		return 'Computer Wins.';
+	}
+	else {
+		playerPointsIncrease();
+		return 'Player Wins.';
+	}
 }
 
 
